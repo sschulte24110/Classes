d0ef85b029342f63f465f17a5f868c888a2119a8
Scott Schulte <scott.r.schulte@gmail.com>
2024-09-03 14:19:08 -0500

    Add Transaction.cs

--- BankAccounts.cs
+++ BankAccounts.cs
@@ -2,10 +2,22 @@ namespace Classes;
 
 public class BankAccount
 {
+  private List<Transaction> _allTransactions = new List<Transaction>();
   private static int s_accountNumberSeed = 1234567890;
   public string Number { get; }
   public string Owner { get; set; }
-  public decimal Balance { get;}
+  public decimal Balance 
+  { 
+    get
+    {
+      decimal balance = 0;
+      foreach (var item in _allTransactions)
+      {
+        balance += item.Amount;
+      }
+      return balance;
+    }
+  }
 
   public BankAccount(string name, decimal initialBalance)
   {
@@ -17,12 +29,26 @@ public class BankAccount
 
   public void MakeDeposit(decimal amount, DateTime date, string note)
   {
-
+    if (amount <= 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+    }
+    var deposit = new Transaction(amount, date, note);
+    _allTransactions.Add(deposit);
   }
 
   public void MakeWithdrawal(decimal amount, DateTime date, string note)
   {
-
+    if (amount <= 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+    }
+    if (Balance - amount < 0) 
+    {
+      throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+    }
+    var withdrawal = new Transaction(-amount, date, note);
+    _allTransactions.Add(withdrawal);
   }
 
 }
--- obj/Debug/net8.0/Classes.AssemblyInfo.cs
+++ obj/Debug/net8.0/Classes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0c03c1c925a41e85445da4b7c60cccbfa93807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b502ba47df20f4b034f2f2352df44f7f823504f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
